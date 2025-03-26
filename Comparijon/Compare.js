7430bbba3dccd481fc37c7bc579e63879bad6cb4
Ismail <ismailhosen8757@gmail.com>
2025-03-26 15:33:00 +0600

    Comparijon

---
+++ Comparijon/Compare.js
@@ -0,0 +1,16 @@
+const money = 50 
+const friendsMoney = "50"
+console.log(money === friendsMoney )
+
+// Practice 2
+console.log('JavaScript' === 'JavaScript')
+// Practice 5
+
+console.log( "Test Condition","test" === 'TEST')
+// CASE secsetioce ,so output false
+
+// Practice 6
+const variableX=15
+const variableY="20"
+
+console.log(variableX !== variableY)
---
+++ Comparijon/TrueFalse.js
@@ -0,0 +1,38 @@
+// Practice 2
+const me = 7
+const brother = 5  
+ console.log("Each Eat me",me > brother)
+
+//  Practice 2 ------ Each Students which Batch
+const myClass = 45 
+const otherClass = 50 
+console.log("Others class Each Student", myClass < otherClass)
+
+// practice 3        Test  Equal
+const myNumber = 75
+const friendsNumber = 75 
+console.log("Check Number ", myNumber == friendsNumber)
+
+// Practice1             page-70
+const brotherAge = 10 
+const myAge = 5 
+console.log("Amar Boyoser theke Kom or Soman kina :",brotherAge >= myAge)
+// Practice 2 
+const PassMark = 33 
+const iGet = 44
+console.log("Ami ki pass koresi" , iGet >= PassMark)
+
+// Prasctice 3
+const jamburaPrice = 30
+const KodBelPrice = 80
+console.log("Jambura kodbeler theke sosta kina", KodBelPrice > jamburaPrice)
+
+// check BisCuit
+const avalabeBiscuit = 15
+const friedsAvailableBuiscuit = 20
+console.log("amar bondur theke kom buiscuit peyesi kina : ",avalabeBiscuit < friedsAvailableBuiscuit)
+
+// check Goal
+const realMadri = 2
+const barsa = 3
+console.log("realMadri ki barsa theke kom gol korese :", realMadri < barsa)
