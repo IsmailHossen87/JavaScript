8daf64d0e8d3cb4538d4b20f99ba07608b344ba5
Ismail <ismailhosen8757@gmail.com>
2025-03-26 11:02:48 +0600

    DataTypeProblemSolving

--- NaN.js
+++ NaN.js
@@ -2,5 +2,41 @@ const age = parseInt("50f")
 const address = parseInt("9Bangladesh")
 const location = parseInt("Bangladesh6")
 console.log(age,address,location)
-
+ 
 // Output   =====   50 9 NaN
+
+// Practice 1     Page-64
+const num = "20"
+const total = parseInt(num) + 10;
+console.log("Practice 1 ",total)
+
+// Practice 2   convert Intiger
+const digit = 3.14159 
+console.log("ConvertIntiger",parseInt(digit))
+
+// Practice 3 
+const value = "PremikBoy"
+console.log("Practice 3",parseInt(value))
+
+// Practice 4
+const value1 = 3.456
+const value2 = 2.789
+const sumation = value1 + value2
+console.log("Parctice 4 ",parseFloat(sumation.toFixed(2)))
+
+// Practice 5 
+const data = "56.78"
+const data1 = "12.34"
+const total1 = parseFloat(data) + parseFloat(data1)
+console.log("Practice 5",total1)
+
+
+// Practice 6 
+const val = "10.5678"
+const val1 = "5.4321"
+const total3 = parseFloat(val) + parseFloat(val1)
+console.log("Practice 6" , total3.toFixed(1))
+
+// Practice 7 
+const numb = "42.3435343456"
+console.log("Practice 7",parseFloat(numb).toFixed(3))
