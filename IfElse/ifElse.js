678cef987831b499d5a2ed8b2faf183a1c321a03
Ismail <ismailhosen8757@gmail.com>
2025-03-29 15:05:23 +0600

    if else

---
+++ IfElse/ifElse.js
@@ -0,0 +1,40 @@
+// Practice 1         page - 77
+const time = 3
+if(time < 6 ){
+    console.log("Mother will make BreakFast")
+}else{
+    console.log("ঝারুর বারি")
+}
+
+// Practice 2 
+const login = false 
+if(login == true){
+    console.log("Welcome")
+}else{
+    console.log("Get Lost")
+}
+
+// Practice 3      
+const run = 6 
+if(run == 5){
+    console.log("Will Give ChoColate")
+}else{
+    console.log("Your fate is increase")
+}
+
+
+// Practice 4
+const mark = 85
+if(mark > 40){
+    console.log("Congretulation,Tumi Bike gift pabe")
+}else{
+    console.log("Sorry")
+}
+
+// Practice 6 
+const temperature = 28
+if(temperature >= 30){
+    console.log("Your start AC")
+}else{
+    console.log("You sleep with katha🤣")
+}
