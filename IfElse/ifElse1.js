6bf1ded3e8b02f13a7c8bd2679f5fda283fe941a
Ismail <ismailhosen8757@gmail.com>
2025-04-01 22:35:14 +0600

    ifElse1

---
+++ IfElse/ifElse1.js
@@ -0,0 +1,91 @@
+// //  Practice 1          page 80
+// const age = 14
+// if(age < 18){
+//     console.log("Will be sitting in the car.")
+// }else{
+//     console.log("The car will hit.")
+// }
+
+
+// // Practice 2 
+// const mathScore = 82
+// const englishScore= 85
+// if(mathScore > 80 || englishScore > 85){
+//     console.log("You will be Admited")
+// }else{
+//     console.log("Tumake biye diye dibe")
+// }
+
+// // Practice 3
+// const gpa =  4
+// const salary = 1009
+// if(gpa >= 5 && salary < 10000){
+//     console.log("He will be Scolership")
+// }else{
+//     console.log("Tumake beton diye porte hobe")
+// }
+
+// // Practice 4
+// const boyos = 28
+// const experience = 3
+// if(boyos < 30 && experience > 2){
+//     console.log("You can take the exam.")
+// }else{
+//     console.log("You can't take the exam.")
+// }
+
+// // Practice 5
+// const countEgg = 3
+// const ciken = true
+// if(countEgg >12  || ciken == false){
+//     console.log("You will be cook eggsKurma.")
+// }else{
+//     console.log("You eat brabe")
+// }
+
+// // Practice 6
+// const temmeprature = 30
+// const Cough = true
+// if(temmeprature > 100 && Cough == true){
+//     console.log("He will go to the doctor.")
+// }else{
+//     console.log("He will sleep at home.")
+
+// }
+
+// // Practice 7
+// const attendence = 34
+// const homeWork =true
+// if(attendence > 80 && homeWork == true){
+//     console.log("You can take exam")
+// }else{
+//     console.log("Fail")
+// }
+
+// // Practice 8
+// const loadShedding = true 
+// const battery = false
+// if(loadShedding == true && battery == false){
+//     console.log("You will be reading Book")
+// }else{
+//     console.log("You will be Playing Virtual Game")
+// }
+
+// // Practice 9
+// const shirtPrcce = 333
+// const kupon = false 
+// if(shirtPrcce > 1000 && kupon == true){
+//     console.log("You will be 20% discount")
+// }
+
+// Practice 10
+// salami joto paicho tar 10% team leader ke dite hobe oi progamm 
+let salami = 6777
+const teamLeader = true 
+if(salami > 1000 && teamLeader == true){
+    let tenPercent = salami * 0.1
+    let total = salami - tenPercent
+    console.log("You will be 10% discount", total)
+}else{
+    console.log("You will be 0% discount",salami)
+}
