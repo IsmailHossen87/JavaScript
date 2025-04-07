3f39495685e052b98a5a6d90f423027b325420ea
Ismail <ismailhosen8757@gmail.com>
2025-04-07 16:38:12 +0600

    childConditon

---
+++ 5thIfElse/ifElse3.js
@@ -0,0 +1,68 @@
+// Practice 1 
+const invite = true 
+const taka = 500
+
+if(invite){
+    console.log("I will go to party")
+  if(taka > 1000){
+    console.log("I will give a gift")
+  }else{
+    console.log("I will go to party but I will not give a gift")
+  }
+}else{
+    console.log("He is not my friend")
+}
+
+// Practice 2
+const wantsTea = true
+const wantsBuiscuit = false
+if(wantsTea){
+    if(wantsBuiscuit){
+        console.log("I will eat biscuit with tea")
+    }else{
+        console.log("I will drink tea only")
+    }  
+}else{
+    console.log("You see star jalsha")
+}
+
+// Practice 3
+const accountActive = true 
+const Premium = true 
+
+if(accountActive){
+    if(Premium){
+        console.log("You can watch the Premium Feature")
+    }else{
+        console.log("Free Feature")
+    }
+}else{
+    console.log("You are not active")
+}
+
+// practice 4
+const availableFood = true  
+const foodPanda = true
+
+if(availableFood){
+    console.log("I will hit the food")
+}else{
+    if(foodPanda){
+        console.log("I will order food")
+    }else{
+        console.log("In the day ,I am fasting.")
+    }
+}
+
+// Practice 5
+const guest = 343
+const gift= true 
+if(guest > 100){
+    if(gift){
+        console.log("Lets Party all night")
+    }else{
+        console.log("I will party with myself")
+    }
+}else{
+    console.log("গেস্ট কম, ছোটখাটো গেট টুগেদার হবে")
+}
